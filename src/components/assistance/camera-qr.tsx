f3a96e761538745872bd804114ceddb951260edb
Autex NI <dev.msobalvarro@gmail.com>
2026-08-12 16:01:51 -0600

    a few changes

--- src/components/assistance/camera-qr.tsx
+++ src/components/assistance/camera-qr.tsx
@@ -1,5 +1,7 @@
 import soundSuccess from '@/assets/sounds/success.mp3'
-import { useDevices, Scanner } from '@yudiel/react-qr-scanner'
+import soundError from '@/assets/sounds/error.mp3'
+import { useSound } from 'use-sound'
+import { useDevices, Scanner, type IDetectedBarcode } from '@yudiel/react-qr-scanner'
 import { useState } from 'react'
 import {
   Select,
@@ -8,10 +10,33 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import { useStudentAssistence } from '@/hooks/API/use-student-assistence'
 
 export const CameraQr = () => {
   const devices = useDevices()
+  const { createAssistence } = useStudentAssistence()
   const [selectedDevice, setSelectedDevice] = useState<MediaDeviceInfo | undefined>(devices[0])
+
+  const [playSuccess] = useSound(soundSuccess)
+  const [playError] = useSound(soundError)
+
+  const onScanQR = async (payload: IDetectedBarcode[]) => {
+    const [result] = payload
+    if (result) {
+      try {
+        await createAssistence({
+          studentId: result.rawValue,
+          date: new Date(),
+          assistence: true
+        })
+
+        playSuccess()
+      } catch (error) {
+        console.warn(error)
+        playError()
+      }
+    }
+  }
 
   return (
     <div className='flex items-center gap-4 flex-col'>
@@ -34,31 +59,13 @@ export const CameraQr = () => {
       </Select>
 
       <Scanner
-        onScan={(result) => console.log(result)}
-        classNames={{
-          container: 'size-[640px]',
-        }}
-        components={{
-          // audio: true, // Play beep sound on scan
-          onOff: true, // Show camera on/off button
-          torch: true, // Show torch/flashlight button (if supported)
-          zoom: false, // Show zoom control (if supported)
-          finder: true, // Show finder overlay
-        }}
+        onScan={onScanQR}
         constraints={{
           deviceId: selectedDevice?.deviceId,
-          width: {
-            exact: 640
+          sampleSize: {
+            ideal: 1920
           },
-          height: {
-            exact: 640,
-          },
-          frameRate: {
-            exact: 30
-          },
-          facingMode: "environment"
         }}
-        sound={soundSuccess}
       />
     </div>
   )
--- src/views/(logged)/institution/assistance/index.tsx
+++ src/views/(logged)/institution/assistance/index.tsx
@@ -1,11 +1,28 @@
 import { CameraQr } from '@/components/assistance/camera-qr'
+import { Button } from '@/components/ui/button'
 import { TitlePageView } from '@/components/ui/title-page'
 import { ViewContainer } from '@/components/ui/view-container'
+import { useStudentAssistence } from '@/hooks/API/use-student-assistence'
 
 export const StudentAssistanceView = () => {
+  const { createAssistence } = useStudentAssistence()
+
   return (
     <ViewContainer>
       <TitlePageView title='Asistencia' subtitle='Registrar la asistencia de los estudiantes' />
+
+      <Button
+        variant='default'
+        onClick={() =>
+          createAssistence({
+            studentId: '6946b6224d5223a2d65f9d81',
+            date: new Date(),
+            assistence: true
+          })
+        }
+      >
+        Crear Asistencia
+      </Button>
 
       <CameraQr />
     </ViewContainer>
